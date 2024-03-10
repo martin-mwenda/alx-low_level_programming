5aff2f8ce819c078e20f3b9bb376fd1075ccf797
martin <mwirigi.8kabush@gmail.com>
2024-03-09 23:13:54 -0800

    changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,10 +9,10 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int decimal = 0;
-	
+
 	if (b == NULL)
 		return (0);
-	
+
 	for (i = 0; b[i] != '\0'; ++i)
 	{
 		if (b[i] != '0' && b[i] != '1')
