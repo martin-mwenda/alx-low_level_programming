3fcea75b8839c2de87d59d6175359b74f1eba546
martin <mwirigi.8kabush@gmail.com>
2024-10-08 01:29:24 -0700

    tasks

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,21 +9,21 @@
  */
 void print_dog(struct dog *d)
 {
-		if (!d->name)
-		{
-			printf("Name: (nil)\n");
-		}
-		else
-		{
-			printf("Name: %s\n", d->name);
-		}
-		printf("Age: %f\n", d->age);
-		if (!d->owner)
-		{
-			printf("owner: (nil)\n");
-		}
-		else
-		{
-			printf("owner: %s\n", d->owner);
-		}
+	if (!d->name)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
+	if (!d->owner)
+	{
+		printf("owner: (nil)\n");
+	}
+	else
+	{
+		printf("owner: %s\n", d->owner);
+	}
 }
